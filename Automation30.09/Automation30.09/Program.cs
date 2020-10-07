52cf58aa875cc99227b403e7cdc548da5d0266ae
LavBod <72502425+LavBod@users.noreply.github.com>
2020-10-07 15:10:58 +0300

    Initial Commit

---
+++ Automation30.09/Automation30.09/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Automation30._09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
